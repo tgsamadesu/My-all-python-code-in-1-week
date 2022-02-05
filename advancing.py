4e56c343a72a71520e78504f8a292f76061e9ddc
TG <98741834+tgsamadesu@users.noreply.github.com>
2022-02-05 18:44:17 +0500

    Add files via upload

---
+++ advancing.py
@@ -0,0 +1,112 @@
+# Calculator :
+''' print ("Enter your fisrt num")
+n1 = input ()
+print ("Enter your second num")
+n2 = input ()
+print ("Your sum of following number is... ", int (n1) + int (n2))'''
+# Dictionary :
+'''d1 = {"Manga":"Whimsical-Pictures", "Nani":"What", "Kami":"God", "Nanda":"Why"}
+print ("Search meaning for... ")
+inp = input ()
+print ("Results...")
+print (d1[inp])'''
+# If-Else-Elif :
+'''var1 = 420
+var2 = int(input())
+if var2>var1:
+    print ("Greater")
+else:
+    print ("Less")'''
+'''print ("Enter your age... ")
+age = int(input())
+if age<7 or age>100:
+    print ("Umm... are you serious??")
+elif age==18:
+    print ("We'll think about it...")
+elif age>18:
+    print ("Yes you can drive!")'''
+# Faulty Calculator:
+'''print ("Enter your number")
+n1 = input ()
+print ("Enter your second number")
+n2 = input ()
+print ("You've entered a invalid number.", "Please Enter a valid number")'''
+# For Loop:
+'''lsit1 = ["8000", "5", "90", "2"]
+print ("Ask")
+inp = input()
+if lsit1>6:
+    print ("It's a valid number")
+else:
+    print ("It's a invalid number")'''
+'''items1 = [20, 29, 20, 190, 6, 3, 8, 9, 2,]
+for item in items1:
+    if str(item).isnumeric and item>6:
+        print (item)'''
+# While loop:
+'''i = 0
+while(i<45):
+    print (i+1)
+    i = i + 1 '''
+'''print("enter a random number")
+c = int(input())
+if c>=100:
+    print(c, "is valid")
+while (c<=100):
+    print("Error input another number")
+    c = int(input())
+    print(c, "is valid")'''
+# My First Succsesful Quiz:
+'''while (True):
+    n1 = int(input))
+        print("Enter your number...\n"))
+    if n1==100:t ("You have entered a valid number\n")
+        break'''
+# A fail exercise:
+'''n1 = input ("Enter your num..." ,)
+n2 = input ("Enter your sec num..." ,) 
+print ("Your answer is", int(n1) * int(n2))'''
+'''while (True):
+    print ("Guess the number. (9 Guesses left")
+    inp = input()
+    if inp==18:
+        print ("You've entered a right number!")
+        break
+    elif inp>18:
+        print ("You've entered the wrong number!")
+        continue
+    else:
+        print ("You have 8 choices ")'''
+# A Successful code of someone:
+'''n=18
+number_of_guesses=1
+print("Number of guesses is limited to only 9 times: ")
+while (number_of_guesses<=9):
+    guess_number = int(input("Guess the number :\n"))
+    if guess_number<18:
+        print("you enter less number please input greater number.\n")
+    elif guess_number>18:
+        print("you enter greater number please input smaller number.\n ")
+    else:
+        print("you won\n")
+        print(number_of_guesses,"no.of guesses he took to finish.")
+        break
+    print(9-number_of_guesses,"no. of guesses left")
+    number_of_guesses = number_of_guesses + 1
+
+if(number_of_guesses>9):
+    print("Game Over")'''
+
+
+
+
+       
+
+   
+        
+        
+
+    
+   
+        
+    
